c9898efb0e40b05d066adc0c1f39bdfdf68bbd8e
gitee <jior2008@163.com>
2018-04-16 14:16:57 +0800

    update

--- src/main/java/com/glaf/framework/system/domain/SysKey.java
+++ src/main/java/com/glaf/framework/system/domain/SysKey.java
@@ -21,6 +21,14 @@ package com.glaf.framework.system.domain;
 import java.io.*;
 import java.util.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import com.alibaba.fastjson.*;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
@@ -38,23 +46,36 @@ import com.glaf.framework.system.domain.util.SysKeyJsonFactory;
  *
  */
 
+@Entity
+@Table(name = "SYS_KEY")
 public class SysKey implements Serializable, JSONable {
 	private static final long serialVersionUID = 1L;
 
+	@Id
+	@Column(name = "ID_", length = 100, nullable = false)
 	protected String id;
 
+	@Column(name = "TITLE_", length = 200)
 	protected String title;
 
+	@Column(name = "NAME_", length = 50)
 	protected String name;
 
+	@Column(name = "TYPE_", length = 50)
 	protected String type;
 
+	@Column(name = "PATH_", length = 200)
 	protected String path;
 
+	@Lob
+	@Column(name = "DATA_")
 	protected byte[] data;
 
+	@Column(name = "CREATEBY_", length = 50)
 	protected String createBy;
 
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "CREATEDATE_")
 	protected Date createDate;
 
 	public SysKey() {
