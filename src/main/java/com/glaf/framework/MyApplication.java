8f6eb300c8a74991e7f4a97b523179f361c3fcbe
jior2008 <jior2008@gmail.com>
2018-07-05 09:48:33 +0800

    update

--- src/main/java/com/glaf/framework/MyApplication.java
+++ src/main/java/com/glaf/framework/MyApplication.java
@@ -21,10 +21,12 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableCaching
 @SpringBootApplication
 @MapperScan("com.glaf.**.mapper")
+@ComponentScan(basePackages = { "com.glaf" })
 public class MyApplication {
 
 	public static void main(String[] args) {
