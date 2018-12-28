f30cdbe05ff07f825d0fab1d223214bf7631f673
jior2008 <jior2008@163.com>
2018-12-28 20:12:37 +0800

    update

--- src/main/java/com/glaf/framework/MyApplication.java
+++ src/main/java/com/glaf/framework/MyApplication.java
@@ -21,7 +21,9 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @EnableCaching
 @SpringBootApplication
@@ -31,6 +33,11 @@ public class MyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
 	}
 
 }
