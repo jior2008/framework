c265716ade6b1d9e0fd131d4acf92c8b27e50f59
jior2008 <jior2008@163.com>
2018-12-31 11:06:51 +0800

    update

--- src/main/java/com/glaf/MyApplication.java
+++ src/main/java/com/glaf/MyApplication.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.glaf.framework;
+package com.glaf;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
